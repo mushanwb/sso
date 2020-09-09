c475649363e74b576d29874860227dfaaa73d736
mushan <49012328+mushanwb@users.noreply.github.com>
2020-09-09 16:15:58 +0800

    微信公众号入口 (#6)
    * 微信公众号入口
    
    用测试公众号测试一下入口连接，也就是绑定的域名是否有效

---
+++ app/Http/Controllers/Wechat/WechatLoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http\Controllers\Wechat;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WechatLoginController extends Controller {
+
+    public function wechatLogin(Request $request) {
+
+        
+
+
+
+    }
+
+
+
+}
---
+++ config/wechatmessage.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return  [
+    //关注
+    "subscribe" => "感谢关注",
+
+    //关键词回复
+    "keyWord" => [
+        "keyWord1" => "没有关键词",
+    ]
+];
