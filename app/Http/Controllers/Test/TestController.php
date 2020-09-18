fe1e3ad73724ab333e6d8ba7ab91281f4be789bd
mushan <49012328+mushanwb@users.noreply.github.com>
2020-09-18 10:55:47 +0800

    更改 token 生成和验证的测试代码 (#7)
    * 更改 token 生成和验证的测试代码
    
    在这个提交中，我更改了 测试代码，主要是对用户传入的数据进行筛选和过滤
    同时我把 token 放入 header 里面验证，后面可以通过写中间键经行拦截

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -13,22 +13,43 @@ use Illuminate\Http\Request;
 class TestController extends Controller {
 
 
+    /**
+     * 测试：token 生成
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function generate(Request $request) {
         $userInfo = User::where('id',83)->first();
 
+        if (!$userInfo) {
+            return response()->json(['code' => 201, 'msg' => '用户信息不存在', 'data' => []]);
+        }
+
         $jwt = new JwtController();
 
-        return $jwt->encrypt($userInfo);
+        $data['token'] = $jwt->encrypt($userInfo);
+
+        return response()->json(['code' => 200, 'msg' => 'success', 'data' => $data]);
     }
 
+    /**
+     * 测试 token 验证
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function verifica(Request $request) {
-        $token = $request->get('token');
+
+        $token = $request->header('token');
 
         $jwt = new JwtController();
 
         $info = $jwt->decrypt($token);
 
-        return $info;
+        if (!$info) {
+            return response()->json(['code' => 201, 'msg' => '验证失败，token 信息错误', 'data' => []]);
+        }
+
+        return response()->json(['code' => 200, 'msg' => '验证成功', 'data' => $info]);
     }
 
 
