f9ce73e60a54f90eb7661e0899500b7813599848
wangbo <1216476406@qq.com>
2020-09-25 17:12:32 +0800

    获取用户信息后添加用户信息到数据库
    通过回调地址中的 code 获取用户信息，如果数据库中没有用户信息
    则将用户信息添加到数据库。否则直接重定向到用户访问的页面。并且带上token

--- app/Http/Controllers/Wechat/WechatLoginController.php
+++ app/Http/Controllers/Wechat/WechatLoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Wechat;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Jwt\JwtController;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use EasyWeChat\Factory;
@@ -20,13 +22,23 @@ class WechatLoginController extends Controller {
     }
 
 
+    /**
+     * 公众号授权
+     * 流程：
+     * 当用户访问一个需要授权页面时，如 baidu.com
+     * 前端需要调用该授权接口进行授权，授权完后微信会调用回调接口
+     * 在回调接口里面保存用户信息，并且重定向到用户要访问的页面：baidu.com
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function wechatAuth(Request $request) {
 
+        // 获取用户要访问页面的 url
         $url = $request->get('url');
         Log::info('重定向的url：' . $url);
 
+        // 将 该重定向的 url 编码后拼接到回调接口中
         $url = '?url=' . urlencode($url);
-
         $this->config['oauth']['callback'] .= $url;
         $app = Factory::officialAccount($this->config);
 
@@ -34,16 +46,63 @@ class WechatLoginController extends Controller {
             ->redirect();
     }
 
+    /**
+     * 授权回调流程：
+     * 该接口为授权回调接口，在授权接口中将该接口地址一起给微信（一般写在配置中）
+     * 用户点击授权后，微信将通过该回调地址将用户的 code 传过来
+     * 由于在授权接口中将用户访问的页面 url 拼接到回调地址后面
+     * 因此微信调用 回调接口的时候，后面会带有 ?url=xxx 等参数
+     * 这些参数可以在回调接口中获取到
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function callback(Request $request) {
         Log::info('获取用户的code:  ' . $request->get('code'));
+
+        //从回调地址中获取需要重定向的 url 参数
         $targetUrl = $request->get('url','/');
-        Log::info('重定向的url：' . $targetUrl);
 
-        $wechatUserInfo = $this->_appNotify->oauth->user();
-        $wechatUserInfo = $wechatUserInfo->original;
-        Log::info('微信返回得用户信息：' . json_encode($wechatUserInfo));
+        // 重定向的 url 上也有可能带有参数，因此需要用 & 判断
+        // 我们需要将生成的 token 放到重定向的 url 中
+        // 前端将会获取 url 中的 token，并且截取下来，放入 header 中
+        // 并且指定 key 为 token （这个值随便，方式需要在取的时候一致），
+        // 方便后端验证 token 的时候取值
+        if (count(explode('&', $targetUrl)) > 1) {
+            $targetUrl = $targetUrl . '&token=';
+        } else {
+            $targetUrl = $targetUrl . '?token=';
+        }
 
-        return redirect()->to($targetUrl);
+        try {
+            $wechatUserInfo = $this->_appNotify->oauth->user();
+            $wechatUserInfo = $wechatUserInfo->original;
+            Log::info('微信返回得用户信息：' . json_encode($wechatUserInfo));
+
+            // 这里使用 openid 判断用户是否注册
+            $userInfo = User::where('openid',$wechatUserInfo['openid'])->first();
+            // 如果用户已经存在，则返回 token 直接登录
+            $jwt = new JwtController();
+            if ($userInfo) {
+                return redirect()->to($targetUrl . 'Bearer'. $jwt->encrypt($userInfo));
+            }
+
+            // 没有则添加用户信息
+            $save = [
+                'openid' => $wechatUserInfo['openid'],
+                'nickname' => $wechatUserInfo['nickname'],
+                'headimgurl' => $wechatUserInfo['headimgurl'],
+                'created_at' => time(),
+                'updated_at' => time()
+            ];
+
+            $id = User::insertGetId($save);
+            $userInfo = User::where('id',$id)->first();
+            return redirect()->to($targetUrl . 'Bearer'. $jwt->encrypt($userInfo));
+        } catch (\Exception $e) {
+            Log::error('用户授权出错：' . $e->getMessage());
+            return redirect()->to($targetUrl);
+        }
+
     }
 
 
