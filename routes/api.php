65061c4a803d76e95c3e0ed323b3d2496ee94707
wangbo <1216476406@qq.com>
2020-09-28 10:39:10 +0800

    add  route_annotate
    添加路由注释

--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // 微信公众号入口
 Route::any('/wechat/entrance','Wechat\WechatApiController@entrance');
 // 微信授权和回调
-Route::any('/wechat/wechatAuth','Wechat\WechatLoginController@wechatAuth');
-Route::any('/wechat/callback','Wechat\WechatLoginController@callback');
+Route::any('/wechat/wechatAuth','Wechat\WechatLoginController@wechatAuth'); // 微信授权
+Route::any('/wechat/callback','Wechat\WechatLoginController@callback'); // 微信授权回调
 
 // 账号密码登录
-Route::post('account/login','Account\AccountLoginController@login');
-Route::post('account/register','Account\AccountLoginController@register');
+Route::post('account/login','Account\AccountLoginController@login');    // 账号登录
+Route::post('account/register','Account\AccountLoginController@register');  // 账号注册
 
 
 // 测试
-Route::get('test/generate','Test\TestController@generate');
-Route::get('test/verifica','Test\TestController@verifica');
+Route::get('test/generate','Test\TestController@generate'); // jwt 生成
+Route::get('test/verifica','Test\TestController@verifica'); // jwt 验证
 
 Route::Group(['middleware' => ['jwt.login']], function () {
-    Route::get('test/user_info','Test\TestController@userInfoNeedLogin');
+    Route::get('test/user_info','Test\TestController@userInfoNeedLogin');   // 通过中间件认证的接口
 });
